cbaff6b32bb4e8709fe76c630958ff0cc30941bb
vynas <hamdabousnina@gmail.com>
2025-05-19 15:20:48 -0400

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,36 @@ import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
 
+const getLocalStorage = () => {
+  let itemList = localStorage.getItem("itemsList");
+  if (itemList) {
+    itemList = JSON.parse(localStorage.getItem("itemsList"));
+  } else {
+    itemList = [];
+  }
+  return itemList;
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("itemsList", JSON.stringify(items));
+};
+
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalStorage());
   const addItem = (itemName) => {
     const newItem = {
       name: itemName,
       completed: false,
       id: nanoid(),
     };
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems);
   };
   return (
     <section className="section-center">
